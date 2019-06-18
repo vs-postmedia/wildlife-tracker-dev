c3b790cd4a49988a12dfe04cfd9b9183a78fc23e
mr_ <njgriffiths@gmail.com>
2019-06-18 09:29:46 -0700

    merged warp timeseries data. basic visual analysis

--- scripts/analysis.r
+++ scripts/analysis.r
@@ -1,31 +1,46 @@
 library(jsonlite)
 library(dplyr)
+library(purrr)
 library(readxl)
 library(janitor)
 library(lubridate)
 
 bcc_data <- read_xlsx('data/bc-conservation/tabula-predatorstatisticsblackbear.xlsx') %>% clean_names()
-warp_data <- fromJSON('data/warp/warp-data-2019.json')
-dl_data <- read.csv('data/encounter-267428-266283.csv')
+# downloaded csv from warp map interface
+# dl_data <- read.csv('data/encounter-267428-266283.csv') 
+warp_json <- fromJSON('data/warp/warp-data-2019.json')
 
-# join so we can create lookup tables for encounter & outcome IDs
-data <- inner_join(warp_data$records, dl_data, by = 'encounter_id') %>%
-  select(-ends_with('.x'))
+# load in all the records in the warp-data folder
+setwd('data/warp/')
+# grab each file in the directory & load it into a list
+warp_json <- list.files() %>% map(fromJSON)
+# back to the main directoru
+setwd('../../')
+
+# extract recrods from list & combine into single dataframe
+df <- data.frame()
+bind_data <- function(d) {
+  tbl = bind_rows(df, d$records)
+}
+
+warp_data <- map_df(warp_json, bind_data)
 
 # extract records & parse year & month into separate columns
-bears <- warp_data$records %>% 
+bear_data <-warp_data %>% 
   mutate(
     year = year(encounter_date),
-    month = month(encounter_date)
+    month = month(encounter_date, label = TRUE)
   )
 
+# join so we can create lookup tables for encounter & outcome IDs
+join_data <- inner_join(warp_data, dl_data, by = 'encounter_id') %>%
+  select(-ends_with('.x'))
 # outcome lookup table
 outcome_lookup <- data %>%
   group_by(outcome_name, encounter_outcome_id) %>% 
   count() %>% 
   na.omit() %>% 
   select(-n)
-    
 # encounter type lookup
 encounter_lookup <- data %>%
   group_by(enctype_name, encounter_enctype_id) %>% 
@@ -41,41 +56,47 @@ encounter_lookup <- data %>%
 ####
 
 # bears by type
-bears %>% 
+bear_data %>% 
   group_by(species_name) %>% 
   count
 
-# by month
-bears %>% 
-  group_by(species_name, month) %>% 
+# by year
+by_year <- bear_data %>% 
+  group_by(species_name, year) %>% 
   count %>% 
-  arrange(-month)
+  arrange(-year)
+
+# by month
+by_month <- bear_data %>% 
+  group_by(species_name, month) %>% 
+  count
 
 # by various data subsets
-loc <- data %>% 
+bear_data %>% 
   # encounter type - 40% sightings, 35% food conditioned
-  # group_by(enctype_name) %>%
+  # group_by(encounter_enctype_id) %>% 
   # outcome - 75% Not specified, 15% advice
   # group_by(outcome_name) %>%
   # location
-  group_by(encounter_locality.y) %>%
+  # group_by(encounter_locality) %>%
   # by attraction - 56% NA, 25% garbage
-  # group_by(attractant_names.y) %>%
+  group_by(attractant_names) %>%
   summarize(
     total = n()
   ) %>% 
   mutate(
     pct = total/sum(total) * 100
   ) %>% 
+  # inner_join(encounter_lookup, by = 'encounter_enctype_id') %>% 
   arrange(-pct)
 
 
 # METRO by attractant
-data %>% 
+metro_attr <- bear_data %>% 
   filter(
-    tolower(encounter_locality.y) %in% metro_van
+    tolower(encounter_locality) %in% metro_van
   ) %>% 
-  group_by(encounter_locality.y, attractant_names.y) %>%
+  group_by(encounter_locality, attractant_names) %>%
   summarize(
     total = n()
   ) %>% 
@@ -83,7 +104,7 @@ data %>%
     pct = total/sum(total) * 100
   ) %>% 
   filter(
-    attractant_names.y != 'NOT APPLICABLE'
+    attractant_names != 'NOT APPLICABLE'
   ) %>% 
   arrange(-total)
 
--- scripts/graphics.r
+++ scripts/graphics.r
@@ -1,2 +1,80 @@
 library(ggplot2)
+library(ggmap)
 library(mapview)
+
+# setup Google API key (w/ sanity check)
+api = "AIzaSyBF-15yhpfXVwqu3VXQ2DA-DO-kTpC6hs8"
+register_google(key = api)
+ggmap_credentials()
+
+gg <- get_googlemap(
+  center = c(-122.926256, 49.246885),
+  maptype = 'terrain'
+  # scale = 2,
+  # zoom = 10
+)
+
+# ggplot() +
+gg +
+  stat_density2d(
+    data = filter(bear_data, tolower(encounter_locality) %in% metro_van),
+    aes(x = encounter_lng, y = encounter_lat, fill = ..level.., alpha = 0.25),
+    size = 0.01, bins = 30,
+    geom = "polygon"
+  ) + 
+  scale_fill_gradient(low="deepskyblue2", high="firebrick1", name="Distribution") +
+  NULL
+
+
+
+# BY YEAR
+by_year %>% 
+  ggplot(aes(
+    x = year, 
+    y = n, 
+    color = species_name,
+    fill = species_name  
+  )) + 
+  geom_bar(stat = 'identity') +
+  # geom_line() + 
+  theme_minimal() +
+  theme(legend.position = 'top') +
+  labs(title = 'Encounters by year')
+  
+
+# BY MONTH
+by_month %>% ggplot(aes(
+  x = month, 
+  y = n, 
+  fill = species_name  
+  )) + 
+  geom_bar(stat = 'identity') +
+  theme_minimal() +
+  labs(title = 'Encounters by month')
+
+
+# BY MUNI/YEAR
+bear_data %>% 
+  filter(
+    tolower(encounter_locality) %in% metro_van
+  ) %>% 
+  group_by(encounter_locality, year, attractant_names, species_name) %>% 
+  summarize(
+    total = n()
+  ) %>%
+  filter(
+    attractant_names == 'GARBAGE'
+  ) %>% 
+  ggplot(aes(
+    x = year, 
+    y = total,
+    color = species_name,
+    fill = species_name  
+  )) + 
+  geom_bar(stat = 'identity') +
+  # geom_line() + 
+  facet_wrap(~encounter_locality) +
+  theme_minimal() +
+  theme(legend.position = 'top') +
+  labs(title = 'Encounters by year')
+   
